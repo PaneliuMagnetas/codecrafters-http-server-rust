e372287a8fcedd61537a5a980e0b4671f0cd377f
Audrius <razmaaudrius@gmail.com>
2023-10-29 21:54:59 +0200

    Pass stage 2

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,6 @@ use std::io::Write;
 use std::net::TcpListener;
 
 fn main() {
-    println!("Logs from your program will appear here!");
-
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
